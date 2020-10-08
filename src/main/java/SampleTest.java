e545b6083a8012b9e92d70f44c7bec1510261343
nowoox <nowoox@gmail.com>
2020-10-08 08:07:36 +0300

    Checking title added

--- src/main/java/SampleTest.java
+++ src/main/java/SampleTest.java
@@ -1,2 +1,52 @@
-public class SampleTest {}
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class SampleTest {
+
+    protected static WebDriver driver;
+    private Logger logger = LogManager.getLogger(SampleTest.class);
+
+    @Before
+    public void setUp(){
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--silent", "--headless");
+        System.setProperty("webdriver.chrome.silentOutput", "true");
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver(options);
+        logger.info("Драйвер поднят");
+
+    }
+
+    @Test
+    public void checkTitle(){
+
+        driver.get("https://otus.ru");
+        logger.info("Страница otus.ru открыта");
+
+        Assert.assertNotNull(driver.getTitle());
+        logger.info("Title страницы проверен");
+
+    }
+
+    @After
+    public void setDown() {
+
+        if (driver != null) {
+            driver.quit();
+
+            logger.info("Драйвер остановлен");
+        }
+
+    }
+}
 
